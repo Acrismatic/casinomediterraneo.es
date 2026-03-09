edf774220a67376dd694c2af9a07558f6e232b38
VictorBlasco5 <victor.blasco.17@gmail.com>
2026-03-09 11:09:35 +0100

    feat: implement evento selection with AJAX for product type

--- src/wp-content/plugins/cm-ecommerce/assets/js/cm-product-type.js
+++ src/wp-content/plugins/cm-ecommerce/assets/js/cm-product-type.js
@@ -47,39 +47,96 @@ jQuery(function($) {
 		$select.data('cmTorneoReady', true);
 	}
 
-	function inheritSimpleVisibilityForTorneo() {
+	function initEventoSelect() {
+		var $select = $('#_cm_evento_id');
+
+		if (!$select.length || !$select.selectWoo || !window.cmEventoSearch) {
+			return;
+		}
+
+		if ($select.data('cmEventoReady')) {
+			return;
+		}
+
+		$select.selectWoo({
+			placeholder: $select.data('placeholder') || 'Selecciona un evento',
+			allowClear: true,
+			minimumInputLength: 0,
+			ajax: {
+				url: cmEventoSearch.ajaxUrl,
+				dataType: 'json',
+				delay: 250,
+				data: function(params) {
+					return {
+						action: cmEventoSearch.action,
+						security: cmEventoSearch.nonce,
+						term: params.term || '',
+						page: params.page || 1
+					};
+				},
+				processResults: function(response, params) {
+					params.page = params.page || 1;
+
+					if (!response || !response.success || !response.data) {
+						return { results: [] };
+					}
+
+					return {
+						results: response.data.results || [],
+						pagination: {
+							more: !!(response.data.pagination && response.data.pagination.more)
+						}
+					};
+				},
+				cache: true
+			}
+		});
+
+		$select.data('cmEventoReady', true);
+	}
+
+	function inheritSimpleVisibilityForCustomTypes() {
 		$('.show_if_simple').each(function() {
 			var $el = $(this);
 
 			if (!$el.hasClass('show_if_torneo-poker')) {
 				$el.addClass('show_if_torneo-poker');
 			}
+
+			if (!$el.hasClass('show_if_evento')) {
+				$el.addClass('show_if_evento');
+			}
 		});
 	}
 
-	function toggleTorneoTab() {
+	function toggleCustomTypeTabs() {
 		var selectedType = $('#product-type').val();
-		var $tab = $('.torneo_info_options');
-		var $panel = $('#torneo_info_product_data');
+
+		$('.torneo_info_options, .evento_info_options').hide();
+		$('#torneo_info_product_data, #evento_info_product_data').hide();
 
 		if (selectedType === 'torneo-poker') {
-			$tab.show();
-			$panel.show();
-		} else {
-			$tab.hide();
-			$panel.hide();
+			$('.torneo_info_options').show();
+			$('#torneo_info_product_data').show();
+		}
+
+		if (selectedType === 'evento') {
+			$('.evento_info_options').show();
+			$('#evento_info_product_data').show();
 		}
 	}
 
-	$(document.body).on('woocommerce-product-type-change', toggleTorneoTab);
-	$('#product-type').on('change', toggleTorneoTab);
+	$(document.body).on('woocommerce-product-type-change', toggleCustomTypeTabs);
+	$('#product-type').on('change', toggleCustomTypeTabs);
 
 	setTimeout(function() {
-		inheritSimpleVisibilityForTorneo();
+		inheritSimpleVisibilityForCustomTypes();
 		initTorneoSelect();
-		toggleTorneoTab();
+		initEventoSelect();
+		toggleCustomTypeTabs();
 		$(document.body).trigger('woocommerce-product-type-change');
 	}, 0);
 
 	$(document.body).on('woocommerce-product-type-change', initTorneoSelect);
+	$(document.body).on('woocommerce-product-type-change', initEventoSelect);
 });
