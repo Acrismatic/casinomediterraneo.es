33220bf131669e165ba964f029c5acc29681cc23
VictorBlasco5 <victor.blasco.17@gmail.com>
2026-03-12 14:49:38 +0100

    feat: add CM Products plugin with initial setup

---
+++ src/wp-content/plugins/cm-products/cm-products.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: CM Products
+ * Description: Extensiones personalizadas.
+ * Version: 1.0.0
+ * Author: Casino Mediterráneo
+ * Text Domain: cm-products
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;  
+}
